da34190ebd5d88dcdca0603f7d1ec38acdcc29e0
RainyXeon / <minh15052008@gmail.com>
2024-03-24 06:55:33 +0700

    Patch meta data

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,11 +1,6 @@
-import fs from 'node:fs';
-
-/** @ignore */
-const packageFileData = JSON.parse(fs.readFileSync('../package.json', { encoding: 'utf-8' }));
-
 /** @ignore */
 export const metadata = {
-	name: packageFileData.name as string,
-	version: packageFileData.version as string,
-	github: packageFileData.repository.url as string,
+	name: 'rainlink',
+	version: '0.5.x',
+	github: 'https://github.com/RainyXeon/ByteBlaze',
 };
