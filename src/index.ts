656dfc8250c7ec01e59749ca4c22be48dc2934a2
RainyXeon / <minh15052008@gmail.com>
2024-03-27 09:49:49 +0700

    Prioritize search from lavalink on Apple, Deezer, Spotify plugin

--- src/index.ts
+++ src/index.ts
@@ -31,5 +31,11 @@ export * from './Interface/Track';
 export * from './Plugin/RainlinkPlugin';
 export * from './Plugin/SourceRainlinkPlugin';
 export { Plugin };
-// Export version
+// Export driver/
+export * from './Drivers/AbstractDriver';
+export * from './Drivers/Lavalink3';
+export * from './Drivers/Lavalink4';
+export * from './Drivers/Nodelink2';
+// Export metadata
+export * from './metadata';
 export const version = metadata.version;
